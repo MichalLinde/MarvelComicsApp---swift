ba1e34930d11fac98d38b0a598e1680960ec18b5
MichalLinde <michal.linde@student.put.poznan.pl>
2022-08-01 09:54:15 +0200

    Initial Commit

---
+++ marvel-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marvel-app
+//
+//  Created by Michal on 01/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
